9ca0aca89db3007ea33c55216cae1848aaa3d77d
Braulio Bhavamitra <braulio@eita.org.br>
2013-10-15 08:37:15 -0300

    Bind keyup and change on select

--- javascripts/escambo.js
+++ javascripts/escambo.js
@@ -6,12 +6,12 @@ escambo_theme = {
 
 escambo_theme.user = {
 
-  select: function(url, new_url) {
+  select: function() {
     var input = jQuery('#select-organization');
     if (input.val() == 'new')
-      window.location = new_url;
+      window.location = this.new_enterprise_url;
     else
-      jQuery.post(url, {profile_id: input.val()});
+      jQuery.post(this.select_organization_url, {profile_id: input.val()});
   },
 };
 
